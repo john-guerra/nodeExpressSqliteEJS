bbd70a2e2588024e5f60f75e7b4d5122ead99e13
John Alexis Guerra Gómez <john.guerra@gmail.com>
2021-06-24 20:01:03 -0700

    Adding bootstrap

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,14 @@ const myDb = require("../db/mySqliteDB.js");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  const references = await myDb.getReferences();
-  console.log("got references" , references);
-
-  res.render("./pages/index", { references });
+  res.redirect("/references");
 });
 
 router.get("/references", async (req, res) => {
   const references = await myDb.getReferences();
-  res.status(200).json(references);
+  console.log("got references" , references);
+
+  res.render("./pages/index", { references });
 });
 
 module.exports = router;
