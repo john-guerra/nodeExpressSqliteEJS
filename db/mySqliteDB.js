6d9ddf534976436e84aca1efefea3907c518e015
John Alexis Guerra Gómez <john.guerra@gmail.com>
2021-07-01 19:02:27 -0700

    Adding pagination

--- db/mySqliteDB.js
+++ db/mySqliteDB.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 
-async function getReferences(query) {
+async function getReferences(query, page, pageSize) {
   console.log("getReferences", query);
 
   const db = await open({
@@ -9,12 +9,52 @@ async function getReferences(query) {
     driver: sqlite3.Database,
   });
 
-  const stmt = await db.prepare(`SELECT * FROM Reference
+  const stmt = await db.prepare(`
+    SELECT * FROM Reference
     WHERE title LIKE @query
-    ORDER BY created_on
-    DESC LIMIT 10;`);
+    ORDER BY created_on DESC
+    LIMIT @pageSize
+    OFFSET @offset;
+    `);
 
-  return stmt.all({ "@query": query + "%" });
+  const params = {
+    "@query": query + "%",
+    "@pageSize": pageSize,
+    "@offset": (page - 1) * pageSize,
+  };
+
+  try {
+    return await stmt.all(params);
+  } finally {
+    await stmt.finalize();
+    db.close();
+  }
+}
+
+async function getReferencesCount(query) {
+  console.log("getReferences", query);
+
+  const db = await open({
+    filename: "./db/database.db",
+    driver: sqlite3.Database,
+  });
+
+  const stmt = await db.prepare(`
+    SELECT COUNT(*) AS count
+    FROM Reference
+    WHERE title LIKE @query;
+    `);
+
+  const params = {
+    "@query": query + "%",
+  };
+
+  try {
+    return (await stmt.get(params)).count;
+  } finally {
+    await stmt.finalize();
+    db.close();
+  }
 }
 
 async function insertReference(ref) {
@@ -27,10 +67,19 @@ async function insertReference(ref) {
     Reference(title, published_on)
     VALUES (@title, @published_on);`);
 
-  return stmt.run({ "@title": ref.title,
-    "@published_on": ref.published_on
-  });
+  try {
+    return await stmt.run({
+      "@title": ref.title,
+      "@published_on": ref.published_on,
+    });
+  } finally {
+    await stmt.finalize();
+    db.close();
+  }
 }
 
 module.exports.getReferences = getReferences;
+module.exports.getReferencesCount = getReferencesCount;
 module.exports.insertReference = insertReference;
+
+
--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,31 @@ router.get("/", async function (req, res, next) {
   res.redirect("/references");
 });
 
+// http://localhost:3000/references?pageSize=24&page=3&q=John
 router.get("/references", async (req, res, next) => {
-
   const query = req.query.q || "";
+  const page = +req.query.page || 1;
+  const pageSize = +req.query.pageSize || 24;
   const msg = req.query.msg || null;
   try {
-
-    let references = await myDb.getReferences(query);
-    res.render("./pages/index", { references, query,  msg });
-
+    let total = await myDb.getReferencesCount(query);
+    let references = await myDb.getReferences(query, page, pageSize);
+    res.render("./pages/index", {
+      references,
+      query,
+      msg,
+      currentPage: page,
+      lastPage: Math.ceil(total/pageSize),
+    });
   } catch (err) {
     next(err);
   }
-
 });
-
 
 router.post("/createReference", async (req, res, next) => {
   const ref = req.body;
 
   try {
-
     const insertRes = await myDb.insertReference(ref);
 
     console.log("Inserted", insertRes);
@@ -36,7 +40,6 @@ router.post("/createReference", async (req, res, next) => {
   } catch (err) {
     console.log("Error inserting", err);
     next(err);
-
   }
 });
 
